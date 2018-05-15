636b0bef689ad7544735bb3c2b79b124d75c8405
Rubén González Alonso <ruben.gonzalezalonso@telefonica.com>
2018-05-15 15:47:25 +0200

    Use behave fixtures instead of before/after methods

--- web_behave/environment.py
+++ web_behave/environment.py
@@ -16,10 +16,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from toolium.behave.environment import (before_all as toolium_before_all, before_feature as toolium_before_feature,
-                                        before_scenario as toolium_before_scenario,
-                                        after_scenario as toolium_after_scenario,
-                                        after_feature as toolium_after_feature, after_all as toolium_after_all)
+from behave import use_fixture
+
+from toolium.behave.environment import before_all_fixture, before_feature_fixture, before_scenario_fixture
 
 
 def before_all(context):
@@ -27,7 +26,7 @@ def before_all(context):
 
     :param context: behave context
     """
-    toolium_before_all(context)
+    use_fixture(before_all_fixture, context)
 
 
 def before_feature(context, feature):
@@ -36,7 +35,7 @@ def before_feature(context, feature):
     :param context: behave context
     :param feature: running feature
     """
-    toolium_before_feature(context, feature)
+    use_fixture(before_feature_fixture, context, feature)
 
 
 def before_scenario(context, scenario):
@@ -45,30 +44,4 @@ def before_scenario(context, scenario):
     :param context: behave context
     :param scenario: running scenario
     """
-    toolium_before_scenario(context, scenario)
-
-
-def after_scenario(context, scenario):
-    """Clean method that will be executed after each scenario
-
-    :param context: behave context
-    :param scenario: running scenario
-    """
-    toolium_after_scenario(context, scenario)
-
-
-def after_feature(context, feature):
-    """Clean method that will be executed after each feature
-
-    :param context: behave context
-    :param feature: running feature
-    """
-    toolium_after_feature(context, feature)
-
-
-def after_all(context):
-    """Clean method that will be executed after all features are finished
-
-    :param context: behave context
-    """
-    toolium_after_all(context)
+    use_fixture(before_scenario_fixture, context, scenario)
